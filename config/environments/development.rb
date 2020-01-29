35e9b36a102523b236d4719f579f5f7e8d32ebfc
zaabii <maiko.uehara@gmail.com>
2020-01-29 20:41:49 +0900

    second commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,8 @@ Rails.application.configure do
     config.action_controller.perform_caching = false
 
     config.cache_store = :null_store
+    
+
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
@@ -59,4 +61,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
 end
